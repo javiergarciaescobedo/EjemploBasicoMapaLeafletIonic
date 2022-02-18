8fa3314625340ed40337cc56e615e5f738f1b55e
Javier García Escobedo <javiergarbedo@gmail.com>
2022-02-18 12:06:38 +0100

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import {Map, Leaflet, tileLayer} from 'leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: L.Map;
+  newMarker:any;
+  address:string[];
 
+  constructor() {
+  }
+
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latitud = 36.6797047;
+    let longitud = -5.4470656;
+    let zoom = 17;
+    this.map = L.map("mapId").setView([latitud, longitud], zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        .addTo(this.map);
+  }
 }
